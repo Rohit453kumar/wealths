7fe0f88b9adccff969a633117d699e1dacddc409
Rohit453kumar <rohit8025kumar@gmail.com>
2025-05-11 11:24:28 +0530

    preparing for deployment

--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -1,11 +1,12 @@
-import { getUserAccounts } from "@/actions/dashboard";
+import { getDashboardData, getUserAccounts } from "@/actions/dashboard";
 import CreateAccountDrawer from "@/components/create-account-drawer";
 import { Card, CardContent } from "@/components/ui/card";
 import { Plus } from "lucide-react";
-import React from "react";
+import React, { Suspense } from "react";
 import AccountCard from "./_components/account-card";
 import { getCurrentBudget } from "@/actions/budget";
 import BudgetProgress from "./_components/budget-progress";
+import DashboardOverview from "./_components/transaction-overview";
 
  async function DashboardPage() {
   const accounts=await getUserAccounts();
@@ -15,7 +16,7 @@ import BudgetProgress from "./_components/budget-progress";
   if (defaultAccount) {
     budgetData = await getCurrentBudget(defaultAccount.id);
   }
-  console.log(budgetData);
+   const transactions=await getDashboardData();
 
   return (
   <div className="space-y=8">
@@ -25,7 +26,15 @@ import BudgetProgress from "./_components/budget-progress";
 currentExpenses={budgetData?.currentExpenses || 0}/>
 )}
 
-    {/*overview*/}
+    {/*Overview*/}
+    <Suspense fallback={"Loading Overview..."}>
+        <DashboardOverview
+        accounts={accounts}
+        transactions={transactions || []}/>
+
+    </Suspense>
+
+
     {/*Account grid*/}
     <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
         <CreateAccountDrawer>
